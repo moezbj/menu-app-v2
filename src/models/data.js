09cb893937ce84a34786c47b031932f78b9b0d91
mahdi <mahdihellal@gmail.com>
2023-06-05 09:49:49 +0100

    hide data

--- src/models/data.js
+++ src/models/data.js
@@ -1,10 +1,11 @@
-import car1 from "../assets/cars/rolls.jpg";
-import car2 from "../assets/cars/urus.jpg";
-import car3 from "../assets/cars/urus_yellow.jpg";
+/* import car360_1 from "../assets/cars/rolls360.jpg";
+import car1 from "../assets/cars/rolls.jpg"; */
 
-import car360_1 from "../assets/cars/rolls360.jpg";
 import car360_2 from "../assets/cars/urus360.jpg";
+import car2 from "../assets/cars/urus.jpg";
+
 import car360_3 from "../assets/cars/urus_yellow360.jpg";
+import car3 from "../assets/cars/urus_yellow.jpg";
 
 import yellow from "../assets/MENU/Yellow.glb";
 import orange from "../assets/MENU/orange3.glb";
